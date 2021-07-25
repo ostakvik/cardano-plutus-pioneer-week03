459f74bf7aa65e16ca5be27724d4368bf69c900a
Odd-Gunnar Stakvik <stakvik@gmail.com>
2021-07-25 17:10:52 +0200

    extended the validator criteria for beneficiary1 to include 'not deadlineReached'. BF1 will now be able to grab the gift only before deadline, not after.

--- src/Week03/Homework1.hs
+++ src/Week03/Homework1.hs
@@ -47,13 +47,13 @@ PlutusTx.unstableMakeIsData ''VestingDatum
 -- or if beneficiary2 has signed the transaction and the deadline has passed.
 mkValidator :: VestingDatum -> () -> ScriptContext -> Bool
 mkValidator dat () ctx =
-    if (signedByBeneficiary $ beneficiary1 dat) then True
+    if ( (signedByBeneficiary $ beneficiary1 dat) && not deadlineReached) then True
     else False
     {-
     if ( (signedByBeneficiary $ beneficiary1 dat ) && deadlineReached == False) then True
     else if ( (signedByBeneficiary $ beneficiary2 dat ) && deadlineReached == True)  then True
     else False-}
-    
+
     where
       info :: TxInfo
       info = scriptContextTxInfo ctx
